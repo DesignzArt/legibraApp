5ad123fb5dfc9d4af5b4de9fee8245631d409bfc
DesignzArt <sammykisinger@gmail.com>
2017-04-02 23:22:29 -0700

    Legibra App by sammy mwangangi
    Find Legibra App by sammy mwangangi

---
+++ LegibraApp/legibra_js/legibraprocess.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function(){
+		$("div#scrollleft").hide("fast");
+	    $("#legibraprocess").click(function(){
+	        $("div#scrollleft").animate({
+	            width: 'toggle'
+
+	        });
+	    });
+})
